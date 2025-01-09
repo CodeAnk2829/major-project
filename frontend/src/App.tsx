1926af2ba40d09895cd32f58cef209a30ea64ecd
Suhaani Batra <suhaanibatra23@gmail.com>
2025-01-10 01:26:22 +0530

    functionality for role specific routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,33 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import PrivateRoute from './components/PrivateRoute'
+import OnlyIssueInchargePrivateRoute from './components/OnlyIssueInchargePrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="sign-up" element={<SignUp/>}/>
+
+        {/* Private Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route path='/' element={<Home/>}/>
+        </Route>
+
+        {/* Issue Incharge Routes */} 
+        <Route element={<OnlyIssueInchargePrivateRoute />}>
+          
+        </Route>
+
+        {/* Admin Routes */}
+        <Route element={<OnlyAdminPrivateRoute />}>
+          
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
