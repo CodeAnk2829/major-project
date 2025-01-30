542f60756ff4ba35190d203f51c89110d2acc591
Suhaani Batra <suhaanibatra23@gmail.com>
2025-01-30 21:18:34 +0530

    create seperate issue incharge sidebar

--- frontend/src/pages/IssueInchargeDashboard.tsx
+++ frontend/src/pages/IssueInchargeDashboard.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import SideBar from '../components/SideBar'
+import InchargeSidebar from '../components/InchargeSidebar';
 
 function IssueInchargeDashboard() {
   return (
     <div className="flex flex-col md:flex-row">
       {/* Sidebar */}
       <div className="w-full md:w-1/4">
-        <SideBar />
+        <InchargeSidebar />
       </div>
 
       {/* Main Section */}
