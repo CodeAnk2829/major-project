fdf4f3cdfaaadd78e1b37f865763202782a19f9d
Suhaani Batra <suhaanibatra23@gmail.com>
2025-02-08 12:02:37 +0530

    modify custom theme for pagination

--- frontend/src/utils/flowbiteCustomThemes.ts
+++ frontend/src/utils/flowbiteCustomThemes.ts
@@ -20,7 +20,7 @@ export const customThemePagination = {
     selector: {
       base: "w-12 border border-gray-300 bg-white py-2 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white",
       active:
-        "bg-[rgb(11,11,11)] text-[rgb(60,79,131)] hover:bg-cyan-100 hover:text-cyan-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white",
+        "bg-[rgb(222,224,244)] text-[rgb(60,79,131)] hover:bg-cyan-100 hover:text-cyan-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white",
       disabled: "cursor-not-allowed opacity-50",
     },
   },
