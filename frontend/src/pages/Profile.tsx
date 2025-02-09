76030489c05211da8c26800366adf00e8c4f8e1e
Suhaani Batra <suhaanibatra23@gmail.com>
2025-02-09 11:13:53 +0530

    fix recent activity

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -6,6 +6,7 @@ import { HiOutlineExclamationCircle } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 import { signOutSuccess } from "../redux/user/userSlice";
 import ComplaintCard from "../components/ComplaintCard";
+import { customThemeTi } from "../utils/flowbiteCustomThemes";
 
 function Profile() {
   const { currentUser } = useSelector((state) => state.user);
@@ -39,7 +40,6 @@ function Profile() {
           phoneNumber: data.phoneNumber,
           role: data.role,
         });
-        console.log(userData);
       }
     } catch (error) {
       setUpdateError("Could not fetch user details: ", error.message);
@@ -54,8 +54,8 @@ function Profile() {
       const res = await fetch("/api/v1/user/me/upvoted");
       const data = await res.json();
       if (data.ok) {
-        setUpvotedComplaints(data.upvotedComplaints);
-        console.log("complaints: ", upvotedComplaints);
+        setUpvotedComplaints(data.upvotedComplaintsDetails);
+        console.log(data.upvotedComplaintsDetails);
       }
     } catch (error) {
       setUpdateError("Could not fetch upvoted complaints: " + error.message);
@@ -172,57 +172,6 @@ function Profile() {
     } catch (error) {
       console.log(error.message);
     }
-  };
-
-  const customThemeTi = {
-    base: "flex",
-    addon:
-      "inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-200 px-3 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-600 dark:text-gray-400",
-    field: {
-      base: "relative w-full",
-      icon: {
-        base: "pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3",
-        svg: "h-5 w-5 text-gray-500 dark:text-gray-400",
-      },
-      rightIcon: {
-        base: "pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3",
-        svg: "h-5 w-5 text-gray-500 dark:text-gray-400",
-      },
-      input: {
-        base: "block w-full border disabled:cursor-not-allowed disabled:opacity-50",
-        sizes: {
-          sm: "p-2 sm:text-xs",
-          md: "p-2.5 text-sm",
-          lg: "p-4 sm:text-base",
-        },
-        colors: {
-          gray: "border-gray-300 bg-gray-50 text-gray-900 focus:border-[rgb(60,79,131)] focus:ring-[rgb(60,79,131)] dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-cyan-500 dark:focus:ring-cyan-500",
-          info: "border-cyan-500 bg-cyan-50 text-cyan-900 placeholder-cyan-700 focus:border-cyan-500 focus:ring-cyan-500 dark:border-cyan-400 dark:bg-cyan-100 dark:focus:border-cyan-500 dark:focus:ring-cyan-500",
-          failure:
-            "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500 dark:border-red-400 dark:bg-red-100 dark:focus:border-red-500 dark:focus:ring-red-500",
-          warning:
-            "border-yellow-500 bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:border-yellow-500 focus:ring-yellow-500 dark:border-yellow-400 dark:bg-yellow-100 dark:focus:border-yellow-500 dark:focus:ring-yellow-500",
-          success:
-            "border-green-500 bg-green-50 text-green-900 placeholder-green-700 focus:border-green-500 focus:ring-green-500 dark:border-green-400 dark:bg-green-100 dark:focus:border-green-500 dark:focus:ring-green-500",
-        },
-        withRightIcon: {
-          on: "pr-10",
-          off: "",
-        },
-        withIcon: {
-          on: "pl-10",
-          off: "",
-        },
-        withAddon: {
-          on: "rounded-r-lg",
-          off: "rounded-lg",
-        },
-        withShadow: {
-          on: "shadow-sm dark:shadow-sm-light",
-          off: "",
-        },
-      },
-    },
   };
 
   const getInitials = (name: string) => {
@@ -399,26 +348,27 @@ function Profile() {
           <h2 className="text-2xl font-semibold text-center mb-4">
             Recent Activity
           </h2>
-
-          {upvotedComplaints && upvotedComplaints.length === 0 ? (
-            <p className="text-gray-500 text-center">
-              No upvoted complaints yet.
-            </p>
-          ) : (
-            <div className="flex flex-col gap-4">
-              {upvotedComplaints &&
-                upvotedComplaints.map((complaint) => (
-                  <ComplaintCard
-                    key={complaint.id}
-                    complaint={complaint.complaint}
-                    showProfile={false}
-                    showUpvote={false}
-                    showActions={false}
-                    showBadges={true}
-                  />
-                ))}
-            </div>
-          )}
+          <div className="max-w-6xl mx-auto flex flex-col gap-8">
+            {upvotedComplaints && upvotedComplaints.length === 0 ? (
+              <p className="text-gray-500 text-center">
+                No upvoted complaints yet.
+              </p>
+            ) : (
+              <div className="flex flex-col gap-4 items-center">
+                {upvotedComplaints &&
+                  upvotedComplaints.map((complaint) => (
+                    <ComplaintCard
+                      key={complaint.id}
+                      complaint={complaint}
+                      showProfile={true}
+                      showUpvote={false}
+                      showActions={false}
+                      showBadges={true}
+                    />
+                  ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
